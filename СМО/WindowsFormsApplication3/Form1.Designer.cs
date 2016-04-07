1a6737bb7ee21e4011a4c2c0f1a0bc0eb3fb9807
Nesterow1 <msdos303@gmail.com>
2016-04-07 18:38:01 +0700

    Beta1

--- СМО/WindowsFormsApplication3/Form1.Designer.cs
+++ СМО/WindowsFormsApplication3/Form1.Designer.cs
@@ -473,8 +473,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.ClientSize = new System.Drawing.Size(414, 276);
             this.Controls.Add(this.tabControl1);
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "СМО";
             this.Load += new System.EventHandler(this.Form1_Load);
